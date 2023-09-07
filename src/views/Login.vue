de60c7e8fc58ed02d7443c4753c4ae498e67b7ff
vickysomtee <vickysomtee@gmail.com>
2023-09-07 13:56:57 +0100

    feat: login scree

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,64 +1,42 @@
 <script setup></script>
 
 <template>
-  <main>
-    <div class="contain">
-      <div class="descrip">
-        <img src="../assets/logo.png" alt="" srcset="" />
-        <h1>Directorate Of Vocation</h1>
+  <main class="flex justify-between h-screen">
+    <section class="banner">
+      <img class="w-[170px] relative top-[50px] left-[110px]" src="../assets/banner.svg" alt="" srcset="" />
+    </section>
+    <section class="w-[60%] bg-white py-[60px] px-[100px]">
+      <div>
+        <h1 class="text-[25px] text-[#061C82] font-black">Log In</h1>
+        <p class="text-[#343434]">Login to your account</p>
       </div>
-      <button class="btn">Log in</button>
-    </div>
+      <form action="" class="mt-[80px]">
+        <div class="grid">
+          <label class="text-[17px] text-[#061C82] font-black" for="email">Email Address</label>
+          <input type="text" class="bg-[#EBEBEB] rounded h-[50px] mt-[12px]" />
+        </div>
+        <div class="grid mt-[40px]">
+          <label class="text-[17px] text-[#061C82] font-black" for="email">Password</label>
+          <input type="text" class="bg-[#EBEBEB] rounded h-[50px] mt-[12px]" />
+        </div>
+        <button class="bg-[#061C82] text-white font-black w-full mt-[50px] py-[12px] rounded-md">
+          Log In
+        </button>
+      </form>
+    </section>
   </main>
 </template>
 
 <style scoped>
-/* body {
-    height: 100vh;
-} */
-main {
-  background-image: url('@/assets/bg.svg');
-  display: flex;
-  justify-content: center;
-  align-items: center;
+/* main {
   height: 100vh;
-  background-position: center;
+   display: flex;
+  justify-content: space-between; 
+} */
+.banner {
+  background-image: url('@/assets/login.svg');
   background-repeat: no-repeat;
   background-size: cover;
-}
-
-.contain {
-  width: 470px;
-  display: grid;
-}
-
-.descrip {
-  display: flex;
-  justify-content: space-between;
-}
-
-img {
-    width: 200px;
-}
-
-h1 {
-  color: white;
-  font-size: 45px;
-  font-weight: bold;
-  width: 250px;
-  padding-top: 30px;
-
-}
-
-.btn {
-  background-color: white;
-  color: #061c82;
-  font-size: 15px;
-  font-weight: bold;
-  border: none;
-  border-radius: 5px;
-  width: 120px;
-  padding: 12px 20px;
-  margin: 20px auto 0;
+  width: 40%;
 }
 </style>

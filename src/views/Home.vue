881d471116aefa92182236b39030e8cc95a03015
vickysomtee <vickysomtee@gmail.com>
2023-09-07 11:02:21 +0100

    feat: homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,13 @@
 
 <template>
   <main>
-    <div class="contain">
-      <div class="descrip">
-        <img src="../assets/logo.png" alt="" srcset="" />
-        <h1>Directorate Of Vocation</h1>
-      </div>
-      <button class="btn">Log in</button>
+    <div class="grid w-[470px]">
+      <img class="w-[450px] mx-auto" src="../assets/banner.svg" alt="" srcset="" />
+      <button
+        class="w-[120px] bg-white text-[15px] font-black mx-auto mt-5 py-2 px-7 rounded text-[#061c82]"
+      >
+        Log in
+      </button>
     </div>
   </main>
 </template>
@@ -25,40 +26,5 @@ main {
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-}
-
-.contain {
-  width: 470px;
-  display: grid;
-}
-
-.descrip {
-  display: flex;
-  justify-content: space-between;
-}
-
-img {
-    width: 200px;
-}
-
-h1 {
-  color: white;
-  font-size: 45px;
-  font-weight: bold;
-  width: 250px;
-  padding-top: 30px;
-
-}
-
-.btn {
-  background-color: white;
-  color: #061c82;
-  font-size: 15px;
-  font-weight: bold;
-  border: none;
-  border-radius: 5px;
-  width: 120px;
-  padding: 12px 20px;
-  margin: 20px auto 0;
 }
 </style>
